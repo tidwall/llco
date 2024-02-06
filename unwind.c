afd8264005ea13abcffcf49decadcfaabe442550
tidwall <joshbaker77@gmail.com>
2024-02-06 08:07:29 -0700

    wip - unwind

--- unwind.c
+++ unwind.c
@@ -98,7 +98,7 @@ __attribute__((noinline)) void func1(int x) { printf("func1\n"); func2(x); }
 void entry(void *udata) {
 
     printf("== COROUTINE ==\n");
-    printf("  frame_address: %p\n", __builtin_frame_address());
+    printf("  frame_address: %p\n", __builtin_frame_address(0));
     func1(10);
     printf("== SWITCH TO MAIN ==\n");
     llco_switch(0, true);
