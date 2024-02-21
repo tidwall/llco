11c5dc0498d894692b2cfe50e0ac73d7782141b2
tidwall <joshbaker77@gmail.com>
2024-02-21 07:23:47 -0700

    Use WIN32 define

--- llco.c
+++ llco.c
@@ -1099,7 +1099,7 @@ const char *llco_method(void *caps) {
     ;
 }
 
-#if defined(__GNUC__) && !defined(__EMSCRIPTEN__) && !defined(_MSC_VER)
+#if defined(__GNUC__) && !defined(__EMSCRIPTEN__) && !defined(_WIN32)
 
 #include <unwind.h>
 #include <string.h>
