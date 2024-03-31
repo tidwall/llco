032192e842c7cb3de4f153dfe7ba358df0b374aa
tidwall <joshbaker77@gmail.com>
2024-03-31 06:13:20 -0700

    Add switch perf to test

--- test.c
+++ test.c
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdint.h>
+#include <time.h>
 #include "llco.h"
 
 #define STKSZ 32768
@@ -62,6 +63,71 @@ void entry1(void *udata) {
     llco_switch(0, true);
 }
 
+void perfcleanup(void *stk, size_t stksz, void *udata) {
+    free(stk);
+}
+
+// Return monotonic nanoseconds of the CPU clock.
+static int64_t getnow(void) {
+    struct timespec now = { 0 };
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    return now.tv_sec * INT64_C(1000000000) + now.tv_nsec;
+}
+
+int64_t perfstart, perfend;
+
+struct llco *perf1co;
+struct llco *perf2co;
+
+#define NSWITCHES 10000000
+int perf_count = 0;
+
+void perf1(void *udata) {
+    perf1co = llco_current();
+    llco_switch(0, 0);
+    while (perf_count < NSWITCHES) {
+        perf_count++;
+        llco_switch(perf2co, 0);
+    }
+    llco_switch(0, 1);
+}
+
+void perf2(void *udata) {
+    perf2co = llco_current();
+    perfstart = getnow();
+    while (perf_count < NSWITCHES) {
+        perf_count++;
+        llco_switch(perf1co, 0);
+    }
+    perfend = getnow();
+    llco_switch(perf1co, 1);
+}
+
+void test_perf() {
+    struct llco_desc desc1 = {
+        .stack = malloc(STKSZ),
+        .stack_size = STKSZ,
+        .entry = perf1,
+        .cleanup = perfcleanup,
+        .udata = (void*)1,
+    };
+    llco_start(&desc1, false);
+
+    struct llco_desc desc2 = {
+        .stack = malloc(STKSZ),
+        .stack_size = STKSZ,
+        .entry = perf2,
+        .cleanup = perfcleanup,
+        .udata = (void*)2,
+    };
+    llco_start(&desc2, false);
+
+    printf("perf: %ld switches in %.3f secs, %ld ns / switch\n", 
+        (long)perf_count, (double)(perfend-perfstart)/1e9, 
+        (long)((perfend-perfstart) / perf_count));
+
+}
+
 int main(void) {
     printf("%s\n", llco_method(0));
     assert(!llco_current());
@@ -92,6 +158,7 @@ int main(void) {
         printf("== expected ==\n%s", exp);
         printf("FAILED\n");
     } else {
+        test_perf();
         printf("PASSED\n");
     }
     return 0;
