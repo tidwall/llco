b6a5a5b886e8a3e06806d741b84571c0b1e1f8d0
Josh <joshbaker77@gmail.com>
2024-04-20 02:07:17 +0000

    Check for GNU_SOURCE
    Fix an issue where dladdr signature is triggering a
    "previous declaration" error.
    
    See https://github.com/tidwall/neco/issues/9

--- llco.c
+++ llco.c
@@ -1126,7 +1126,8 @@ struct llco_dlinfo {
     void            *dli_saddr;     /* Address of nearest symbol */
 };
 
-#ifdef __linux__
+
+#if defined(__linux__) && !defined(_GNU_SOURCE) 
 int dladdr(const void *, void *);
 #endif
 
